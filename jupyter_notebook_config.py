013230210474096144d0dd15ab19bdc1edb2fe63
medicharlachiranjeevi <medicharlachiranjeevi@gmail.com>
2021-02-02 16:18:59 +0530

    Update jupyter_notebook_config.py

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -1,2 +1,3 @@
+c.MappingKernelManager.cull_idle_timeout = 600
+c.NotebookApp.shutdown_no_activity_timeout = 600
 c.NotebookApp.tornado_settings = {'headers': {'Content-Security-Policy': "frame-ancestors * self "}}
-c.NotebookApp.shutdown_no_activity_timeout = 540
